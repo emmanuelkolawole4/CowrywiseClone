dcd3c7e773526aff2c1b05e67bb7bbdb84864e09
emmanuelkolawole4 <emmanuelkolawole4@gmail.com>
2020-09-28 11:53:19 +0100

    Initial Commit

---
+++ CowryWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CowryWise
+//
+//  Created by FOLAHANMI KOLAWOLE on 28/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
